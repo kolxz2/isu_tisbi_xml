5f4f117f6252f297f8b28ad4e9a9f16a7c414216
Nike <67540774+kolxz2@users.noreply.github.com>
2023-07-04 14:15:12 +0300

    data is saved on first login: studentHashValue, login, password, regIdValue
    (cherry picked from commit 4a4a77f354c002cdcc400c7b2e542e57062c2871)

--- app/src/main/java/ru/nikolas_snek/isu_tisbi_xml/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/ru/nikolas_snek/isu_tisbi_xml/data/repository/UserRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package ru.nikolas_snek.isu_tisbi_xml.data.repository
 
 import androidx.datastore.core.IOException
-import kotlinx.coroutines.flow.first
 import okhttp3.ResponseBody
 import ru.nikolas_snek.isu_tisbi_xml.data.TempUserApi
 import ru.nikolas_snek.isu_tisbi_xml.data.api.ApiAuthService
@@ -18,33 +17,24 @@ class UserRepositoryImpl(
     private val apiAuthService: ApiAuthService,
     private val preferences: UserPreferences,
 ) : BaseRepository() {
-
-    suspend fun refreshData(): String {
-        val login = preferences.loginStudent.first()!!
-        val password = preferences.passwordStudent.first()!!
-        val peopleRoleId = preferences.peopleRole.first()!!
-        val tokenValue = checkSuccess(obtainTempToken(login, password))
-        val personalTokenValue = checkSuccess(obtainPersonalToken(tokenValue, peopleRoleId))
-        TempUserApi.personalAuthToken = personalTokenValue
-        return personalTokenValue
-    }
-
     suspend fun login(login: String, password: String): ResultRequest<String> {
+
+        val tempToken = obtainTempToken(login, password)
         // todo почистить
-        val tokenValue = checkSuccess(obtainTempToken(login, password))
+        val tokenValue = checkSuccess(tempToken)
         preferences.saveToken(tokenValue)
-        val peopleRoleId = checkSuccess(obtainRegId(tokenValue))
-        val personalTokenValue = checkSuccess(obtainPersonalToken(tokenValue, peopleRoleId))
+        val regIdValue = checkSuccess(obtainRegId(tokenValue))
+        val personalTokenValue = checkSuccess(obtainPersonalToken(tokenValue, regIdValue))
         val studentHashValue = checkSuccess(obtainStudentHash(personalTokenValue))
         TempUserApi.personalAuthToken = personalTokenValue
         preferences.saveStudentHash(studentHashValue)
         preferences.saveLogin(login)
         preferences.savePassword(password)
-        preferences.savePeopleRole(peopleRoleId)
-        /*        Log.d("NET", "tokenValue $tokenValue")
-                Log.d("NET", "regIdValue $regIdValue")
-                Log.d("NET", "personalTokenValue $personalTokenValue")
-                Log.d("NET", "studentHashValue $studentHashValue")*/
+        preferences.savePeopleRole(regIdValue)
+/*        Log.d("NET", "tokenValue $tokenValue")
+        Log.d("NET", "regIdValue $regIdValue")
+        Log.d("NET", "personalTokenValue $personalTokenValue")
+        Log.d("NET", "studentHashValue $studentHashValue")*/
         return safeApiCall {
             val tokenResponse =
                 apiAuthService.loginPost(LoginRequest(login, password)).execute().body()
@@ -75,10 +65,7 @@ class UserRepositoryImpl(
 
     }
 
-    private suspend fun obtainPersonalToken(
-        tokenValue: String,
-        regIdValue: Int,
-    ): ResultRequest<String> {
+    private suspend fun obtainPersonalToken(tokenValue: String, regIdValue: Int): ResultRequest<String>{
         return safeApiCall {
             val response =
                 apiAuthService.roleUsePost(
@@ -98,8 +85,7 @@ class UserRepositoryImpl(
                     personalTokenValue
                 ).execute().body()
             response?.studentHash
-                ?: throw IllegalStateException("Login response is null or token is missing")
-        }
+                ?: throw IllegalStateException("Login response is null or token is missing")        }
     }
 
     suspend fun saveAuthToken(token: String) {
